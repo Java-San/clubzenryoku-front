11c04434a664a46d24114f0c33665c836d8b5534
Javiera Quiñones <javiera.sandoval@devaid.cl>
2022-07-19 22:06:35 -0400

    WP: avanzando en la pagina provisoria.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { ProvisoryComponent } from './components/provisory/provisory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvisoryComponent
   ],
   imports: [
     BrowserModule,
